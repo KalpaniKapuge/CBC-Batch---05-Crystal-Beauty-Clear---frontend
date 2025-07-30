19e5c8fcfd8e5bf6846f7e81ee0d7580f000bb4d
KalpaniKapuge <kalpanikapuge1020@gmail.com>
2025-07-30 17:27:07 +0530

    Implement client product page✏️

---
+++ pages/client/productPage.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+
+export default function ProductPage(){
+    const [products,setProducts] = useState([])
+    const [isLoading,setIsLoading] = useState(true)
+
+    useEffect(
+        () => {
+            if(isLoading){
+                axios.get(import.meta.env.VITE_BACKEND_URL+"/api/products").then(
+                    (res) => {
+                        setProducts(res.data)
+                        setIsLoading(false)
+                    }
+                )
+            }
+        },[isLoading]
+    )
+
+    return(
+        <div className="w-full h-full flex flex-wrap jsutify-center items-center">
+            {
+                products.map((product) => {
+                    return(
+                        <ProductCard key={product.productId} product={product}/>
+                    )
+                })
+            }
+        </div>
+    )
+    
+}
