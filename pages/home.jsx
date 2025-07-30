a78f7bf95e3bf6fcb826c279dbc7307d2385ab9f
KalpaniKapuge <kalpanikapuge1020@gmail.com>
2025-07-30 14:40:31 +0530

    Update Home Page✏️

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,22 +1,25 @@
+import AdminProductsPage from '../admin/adminProductsPage.jsx';
 import ProductCard from '../src/components/productCard.jsx';
 
 export default function HomePage() {
   return (
-    <div className="home-page">
-      <h1>Welcome to Crystal Bloom</h1>
-      <p>Your one-stop shop for beauty products!</p>
-      <ProductCard
-        name="Lipstick"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ab praesentium qui quam ducimus cum sit dignissimos beatae omnis fugit!"
-        price="1000/="
-        picture="https://png.pngtree.com/png-vector/20221014/ourmid/pngtree-3dc4d-stereo-lipstick-png-image_6150416.png"
-      />
-      <ProductCard
-        name="Eyeliner"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ab praesentium qui quam ducimus cum sit dignissimos beatae omnis fugit!"
-        price="2000/="
-        picture="https://www.laurenbrookecosmetiques.com/cdn/shop/files/Liquid_Eyeliner_2024_sm.png?v=1743546015"
-      />
+    <div className='w-full h-screen flex flex-col items-center justify-center'>
+      <Header/>
+      <div className='w-full h-[calc(100vh-80px)] flex flex-col items-center'>
+        
+          
+          <Routes path="/">
+          <Route path="/" element={<h1>Home</h1>}/>
+          <Route path="/products" element={<AdminProductsPage/>} />
+          <Route path="/contact" element={<h1 className='text-2xl font-bold'>Contact</h1>} />
+          <Route path="/*" element={<h1 className='text-2xl font-bold'>404 Not Found</h1>} />
+          </Routes>
+          
+
+
+      </div>
+
     </div>
+
   );
 }
